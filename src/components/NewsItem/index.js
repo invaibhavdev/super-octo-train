94f4d1bb46308ed278be243b1b5b45edb0ab3246
invaibhavdev <invaibhavdev@gmail.com>
2020-07-26 23:48:43 +0530

    Front page is now responsive

--- src/components/NewsItem/index.js
+++ src/components/NewsItem/index.js
@@ -6,11 +6,18 @@ import getUrlHost from 'utils/getUrlHost';
 import Button from 'components/UI/Button';
 
 const News = styled.div`
-  @media only screen and (min-width: 768px) {
-  	div {
-/*  		display: none;
-*/ 	}
-  }
+	@media only screen and (max-width: 767px) {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		>button {
+			width: 48px;
+			height: 48px;
+		}
+	  	.only-desk {
+			display: none;
+	 	}
+	}
 `;
 
 const Link = styled.a`
@@ -26,22 +33,46 @@ const Subtext = styled.span`
 	padding: 0 4px;
 `;
 
-const Author = styled(Subtext)`
+const SubtextDark = styled(Subtext)`
 	color: #000;
+`;
+const NewsInfo = styled.div`
+	display: flex;
+	.visible-phone-inline {
+		display: none;
+	}
+	text-align: left;
+	@media only screen and (max-width: 767px) {
+	.visible-phone-inline {
+		display: inline-block;
+	}
+		flex-direction: column;
+	}
+
 `;
 
 export default (props) => (
 	<News className="Grid-head">
-		<span>{props.comments}</span>
-		<span>{props.votes}</span>
-		<Button><BsFillTriangleFill color="#9d9e98" /></Button>
-		<div>
-			<Link target="_blank" rel="noopener noreferrer" href={props.url}>{props.title}</Link>
-			<Subtext>({getUrlHost(props.url)})</Subtext>
-			<Subtext>by</Subtext>
-			<Author>{props.author}</Author>
-			<Subtext>{dateFormat(props.createdAt)}</Subtext>
-			<Button><Author>[hide]</Author></Button>
-		</div>
+		<SubtextDark className="only-desk">{props.comments}</SubtextDark>
+		<SubtextDark className="only-desk">{props.votes}</SubtextDark>
+		<Button>
+			<BsFillTriangleFill color="#9d9e98" />
+		</Button>
+		<NewsInfo>
+			<div>
+				<Link target="_blank" rel="noopener noreferrer" href={props.url}>{props.title}</Link>
+				<Subtext>({getUrlHost(props.url)})</Subtext>
+			</div>
+			<div>
+				<div className="visible-phone-inline">
+					<SubtextDark>{props.comments} comments</SubtextDark>
+					<SubtextDark>{props.votes} points</SubtextDark>
+				</div>
+				<Subtext>by</Subtext>
+				<SubtextDark>{props.author}</SubtextDark>
+				<Subtext>{dateFormat(props.createdAt)}</Subtext>
+				<Button><SubtextDark>[hide]</SubtextDark></Button>
+			</div>
+		</NewsInfo>
 	</News>
 )
